8b42a28ea8a2fdbe9190810e52ac6786e022ee7d
Paul Jones <1019888+SenojLuap@users.noreply.github.com>
2021-07-09 15:33:41 -0400

    finished compress()

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::path;
 use std::io;
 use std::fs;
+use std::collections::HashMap;
 
 pub enum CompressError {
     IoError(io::Error),
@@ -15,14 +16,34 @@ impl From<io::Error> for CompressError {
 pub fn compress_file(in_file: &path::Path, out_file: &path::Path) -> Result<(), CompressError> {
 
     let mut buffer = vec![];
+    let mut output = vec![];
+
+    let mut dictionary = HashMap::new();
 
     for byte in fs::read(&in_file)? {
         buffer.push(byte);
         if buffer.len() == 1 { // All single-byte sequences are assumed to be in the dictionary
             continue;
         }
-        
+        if dictionary.contains_key(&Vec::from(&buffer[..])) {
+            continue;
+        }
+        dictionary.insert(Vec::from(&buffer[..]), (dictionary.len()+256) as u32);
+
+        let new_byte = buffer.pop().unwrap(); // Earlier checks make this impossible to be 'None'.
+        let old_code = if buffer.len() == 1 {
+                buffer.pop().unwrap() as u32
+            } else {
+                dictionary.get(&buffer[..]).unwrap().clone()
+            };
+        for byte in old_code.to_be_bytes() {
+            output.push(byte);
+        }
+        output.push(new_byte);
+        buffer.clear();
     }
+
+    fs::write(out_file, &output[..])?;
 
     Ok(())
 }
@@ -32,6 +53,8 @@ pub fn compress_file(in_file: &path::Path, out_file: &path::Path) -> Result<(), 
 mod tests {
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let left = vec![1, 2, 3];
+        let right = vec![1, 2, 3];
+        assert_eq!(left, right);
     }
 }
