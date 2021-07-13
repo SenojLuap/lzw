ef296b432b64d159288976d6bcc430a4a4bc39f3
Paul Jones <1019888+SenojLuap@users.noreply.github.com>
2021-07-13 13:06:52 -0400

    compress binary

---
+++ src/bin/lzwcompress.rs
@@ -0,0 +1,53 @@
+use std::env;
+use std::process;
+use std::path::Path;
+use lzw::{self, CompressError};
+
+struct Config {
+    file_name: String,
+    output_file: String
+}
+
+pub fn main() {
+    let config = match parse_args(env::args()) {
+        Ok(config) => config,
+        Err(msg) => {
+            eprintln!("{}", msg);
+            process::exit(1);
+        }
+    };
+
+    let compress_result = lzw::compress_file(Path::new(&config.file_name), Path::new(&config.output_file), 2);
+    if let Err(err) = compress_result {
+        match err {
+            CompressError::IoError(io_error) => {
+                eprintln!("IO Error: {}", io_error);
+                process::exit(1);
+            }
+            CompressError::InternalError(msg) => {
+                eprintln!("Internal error: {}", msg);
+                process::exit(1);
+            }
+        }
+    }
+}
+
+
+fn parse_args(mut args: env::Args) -> Result<Config, &'static str> {
+    args.next();
+
+    let file_name = match args.next() {
+        Some(file_name) => file_name,
+        None => return Err("Must provide name of file to compress")
+    };
+
+    let output_file = match args.next() {
+        Some(output_file) => output_file,
+        None => return Err("Must specify output file name/location")
+    };
+
+    Ok(Config {
+        file_name: file_name,
+        output_file: output_file
+    })
+}
